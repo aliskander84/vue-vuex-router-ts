eb3578cea81f634ae8172d14cb7c760ea185b19e
alexchegash <aliskander84@gmail.com>
2020-04-01 13:17:24 +0300

    create loader component

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,6 +1,19 @@
 <template>
-  <v-card>
-    UserCard
+  <v-card class="mx-3">
+    <v-img
+        :src="pic"
+        width="200px"
+    />
+    <v-card-title>
+      Title
+    </v-card-title>
+    <v-card-subtitle>
+      Subtitle
+    </v-card-subtitle>
+    <v-card-text>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deserunt dolor eligendi eveniet excepturi
+      maxime quibusdam quisquam sapiente tempora voluptate.
+    </v-card-text>
   </v-card>
 </template>
 
@@ -8,6 +21,12 @@
   import Vue from 'vue'
 
   export default Vue.extend({
-    name: 'UserCard'
+    name: 'UserCard',
+    props: {
+      id: String,
+      pic: String,
+      firstName: String,
+      lastName: String
+    }
   })
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,39 @@
 <template>
   <div class="home">
     <Search/>
-    <UserCard/>
+    <Loader/>
+    <!--    <UserCard-->
+<!--      :id="users[0].id.name"-->
+<!--      :pic="users[0].picture.large"-->
+<!--      :first-name="users[0].name.first"-->
+<!--      :last-name="users[0].name.last"-->
+<!--    />-->
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import Loader from '@/components/Loader.vue'
   import Search from '@/components/Search.vue'
   import UserCard from '@/components/UserCard.vue'
+  import {TUsers} from '@/store/types'
 
   export default Vue.extend({
     name: 'Home',
-    mounted(): void {
-      fetch('https://randomuser.me/api/?results=20&inc=id,picture,name')
-      .then(res => res.json())
-      .then(data => console.log(data))
+    data() {
+      return {
+        users: [] as TUsers
+      }
+    },
+    async mounted() {
+      const res = await fetch('https://randomuser.me/api/?results=20&inc=id,picture,name')
+      const users: TUsers = await res.json()
+      this.users = users
     },
     components: {
+      Loader,
       Search,
-      UserCard
+      // UserCard
     }
   })
 </script>
